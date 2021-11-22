ab74b5073c2943fb4277130cdcd908e68075c2d0
elantsev1 <elantsev.pa@gmail.com>
2021-11-22 13:26:12 +0300

    commit1

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { store } from './app/store';
 import './index.css';
+// commit1
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
